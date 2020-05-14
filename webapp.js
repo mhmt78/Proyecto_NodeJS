9f172fe1236971fbf623f214596079554f53618f
mhmt78 <mhernan157@hotmail.com>
2020-05-14 17:25:21 -0500

    GET Autores y autores.id

--- webapp.js
+++ webapp.js
@@ -14,35 +14,37 @@ var pool = mysql.createPool({
 aplicacion.use(bodyParser.json())
 aplicacion.use(bodyParser.urlencoded({ extended: true }))
 
+// GET /api/v1/publicaciones
+// GET /api/v1/publicaciones?busqueda=<palabra>
 aplicacion.get('/api/v1/publicaciones/', function(peticion, respuesta) {
   pool.getConnection(function(err, connection) {
-  let query
-  const busqueda = (peticion.query.busqueda) ? peticion.query.busqueda : ""
-  if (busqueda == "") {
-  query = `SELECT * FROM publicaciones`
-  } else {
-  query = ` SELECT * FROM publicaciones WHERE
-  titulo LIKE '%${busqueda}%' OR
-  resumen LIKE '%${busqueda}%' OR
-  contenido LIKE '%${busqueda}%'
-  `
-  }
-  connection.query(query, function(error, filas, campos) {
-  if (filas.length > 0) {
-  respuesta.json({ data: filas })
-  } else {
-  respuesta.status(404)
-  respuesta.send({ errors: ["No se encuentra esa publicacion"] })
-  }
-  })
-  connection.release()
-  })
+    let query
+    const busqueda = (peticion.query.busqueda) ? peticion.query.busqueda : ""
+    if (busqueda == "") {
+      query = `SELECT * FROM publicaciones`
+    } else {
+      query = ` SELECT * FROM publicaciones WHERE
+      titulo LIKE '%${busqueda}%' OR
+      resumen LIKE '%${busqueda}%' OR
+      contenido LIKE '%${busqueda}%'
+      `
+    }
+    
+    connection.query(query, function(error, filas, campos) {
+      if (filas.length > 0) {
+        respuesta.json({ data: filas })
+      } else {
+        respuesta.status(404)
+        respuesta.send({ errors: ["No se encuentra esa publicacion"] })
+      }
+    })
+    connection.release()
+    })
   })
 
+//GET /api/v1/publicaciones/<id>
 aplicacion.get('/api/v1/publicaciones/:id', function (peticion, respuesta) {
-
   pool.getConnection(function(err, connection) {
-
     const query = `SELECT * FROM publicaciones WHERE id=${connection.escape(peticion.params.id)}`
     connection.query(query, function (error, filas, campos) {
       if (filas.length > 0){
@@ -52,12 +54,37 @@ aplicacion.get('/api/v1/publicaciones/:id', function (peticion, respuesta) {
         respuesta.status(404)
         respuesta.send({errors: ["No se encuentra esa tarea"]})
       }
-
     })
     connection.release()
-
   })
+})
 
+//GET /api/v1/autores
+aplicacion.get('/api/V1/autores', function (peticion, respuesta) {
+  pool.getConnection(function(err, connection) {
+    const query = `SELECT * FROM autores`
+    connection.query(query, function (error, filas, campos) {
+      respuesta.json({data: filas})
+    })
+    connection.release()
+  })
+})
+
+// GET /api/v1/autores/<id>
+aplicacion.get('/api/v1/autores/:id', function (peticion, respuesta) {
+  pool.getConnection(function(err, connection) {
+    const query = `SELECT * FROM autores WHERE id=${connection.escape(peticion.params.id)}`
+    connection.query(query, function (error, filas, campos) {
+      if (filas.length > 0){
+        respuesta.json({data: filas[0]})
+      }
+      else{
+        respuesta.status(404)
+        respuesta.send({errors: ["No se encuentra el autor"]})
+      }
+    })
+    connection.release()
+  })
 })
 
 aplicacion.listen(8080, function(){
